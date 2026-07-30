d6ce1ec9ea97fc6538e1f86c7b5157d4c833a36f
mehadishisir <mehadishisir@gmail.com>
2026-07-31 02:45:32 +0600

    add basic navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,16 +1,86 @@
 import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import logo from "@/public/logo.png"
 
 export default function Navbar() {
   return (
-    <nav className="border-b p-4">
-      <div className="mx-auto flex max-w-7xl items-center justify-between">
-        <h1 className="text-2xl font-bold">🏋️ GearUp</h1>
+    <nav className="sticky top-0 z-50 border-b bg-white/90 shadow-lg backdrop-blur">
+      <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-5">
 
-        <div className="flex gap-6">
-          <Link href="/">Home</Link>
-          <Link href="/gear">Gear</Link>
-          <Link href="/auth/login">Login</Link>
+        {/* Logo */}
+        <Link href="/" className="flex items-center">
+          <Image
+            src={logo}
+            alt="GearUp Logo"
+            width={140}
+            height={45}
+            priority
+          />
+        </Link>
+
+
+        {/* Nav Links */}
+        <div className="hidden items-center gap-8 md:flex">
+          {NAV_LINKS.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="
+              font-medium
+              text-slate-700
+              transition
+              duration-300
+              hover:text-orange-500
+              "
+            >
+              {item.label}
+            </Link>
+          ))}
         </div>
+
+
+        {/* Auth Buttons */}
+        <div className="hidden items-center gap-3 md:flex">
+
+          <Link
+            href="/auth/login"
+            className="
+            rounded-xl
+            border
+            border-slate-300
+            px-5
+            py-2
+            font-medium
+            text-slate-700
+            transition
+            hover:border-orange-500
+            hover:text-orange-500
+            "
+          >
+            Login
+          </Link>
+
+
+          <Link
+            href="/auth/register"
+            className="
+            rounded-xl
+            bg-orange-500
+            px-5
+            py-2
+            font-medium
+            text-white
+            shadow-md
+            transition
+            hover:bg-orange-600
+            "
+          >
+            Register
+          </Link>
+
+        </div>
+
       </div>
     </nav>
   );
---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Gear",
+    href: "/gear",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
